0860bc21c26ac1ba58572ab0f59e101a00287ce4
Ilia Yusov <ouster2k@gmail.com>
2021-03-24 02:02:53 +0300

    added admin panel

--- api_app/admin.py
+++ api_app/admin.py
@@ -4,19 +4,41 @@ from api_app.models import ProductCollection, Product, Order, ProductReview
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'price', 'created_at')
+    list_display_links = ('id', 'name', 'price', 'created_at')
+    list_filter = ('name', )
+
+
+class PositionsInline(admin.TabularInline):
+    model = Order.products.through
 
 
 @admin.register(ProductReview)
 class ProductReviewAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'product', 'user', 'created_at')
+    list_display_links = ('id', 'product', 'user', 'created_at')
+    list_filter = ('user', 'created_at', ('product', admin.RelatedOnlyFieldListFilter))
+
+
+def product_quantity(obj):
+    return len(obj.products.all())
+
+
+product_quantity.short_description = 'Кол-во товаров'
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'user', product_quantity, 'status', 'created_at')
+    list_display_links = ('id', 'user', product_quantity, 'status', 'created_at')
+    list_filter = ('user', 'created_at')
+    ordering = ('-created_at', )
+    inlines = [PositionsInline]
 
 
 @admin.register(ProductCollection)
 class ProductCollectionAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'name', 'text', 'created_at')
+    list_display_links = ('id', 'name', 'created_at')
+    list_filter = ('name', )
+    filter_horizontal = ('products', )
--- api_app/models.py
+++ api_app/models.py
@@ -4,77 +4,170 @@ from django.db import models
 
 
 class OrderStatusChoices(models.TextChoices):
-    """Статусы объявления."""
+    """Статусы заказов."""
     NEW = "NEW", "Новый"
     IN_PROGRESS = "IN_PROGRESS", "В работе"
     DONE = "DONE", "Закрыт"
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField(blank=True, default='')
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    """Модель Товары"""
+    name = models.CharField(
+        max_length=128,
+        verbose_name='Название'
+    )
+    description = models.TextField(
+        blank=True,
+        default='',
+        verbose_name='Описание'
+    )
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        verbose_name='Цена'
+    )
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name='Дата создания'
     )
     updated_at = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
+        verbose_name='Дата обновления'
     )
 
     def __str__(self):
-        return f'{self.id}, {self.name}'
+        return f'ID_{self.id} - {self.name}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
 
 
 class OrderPositions(models.Model):
-    product = models.ForeignKey(Product,
-                                related_name='orders',
-                                on_delete=models.CASCADE)
-    order = models.ForeignKey('Order',
-                              related_name='positions',
-                              on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
+    """Промежуточная модель Позиций в заказе"""
+    product = models.ForeignKey(
+        Product,
+        related_name='orders',
+        on_delete=models.CASCADE,
+        verbose_name='Товар'
+    )
+    order = models.ForeignKey(
+        'Order',
+        related_name='positions',
+        on_delete=models.CASCADE,
+        verbose_name='Заказ'
+    )
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name='Количество'
+    )
 
 
 class ProductReview(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE, )
-    text = models.TextField(blank=True, default='')
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    """Модель Отзывы на товары"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    product = models.ForeignKey(
+        Product,
+        related_name='reviews',
+        on_delete=models.CASCADE,
+        verbose_name='Товар'
+    )
+    text = models.TextField(
+        blank=True,
+        default='',
+        verbose_name='Текст отзыва')
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        verbose_name='Оценка'
+    )
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name='Дата создания'
     )
     updated_at = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
+        verbose_name='Дата обновления'
     )
 
-    class Meta:
+    def __str__(self):
+        return f'ID_{self.id} - ({self.product}), {self.user}'
 
+    class Meta:
         unique_together = ['user', 'product']
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, through=OrderPositions)
+    """Модель Заказы"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    products = models.ManyToManyField(
+        Product,
+        through=OrderPositions,
+        verbose_name='Позиции'
+    )
     status = models.TextField(
         choices=OrderStatusChoices.choices,
-        default=OrderStatusChoices.NEW
+        default=OrderStatusChoices.NEW,
+        verbose_name='Статус заказа'
     )
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Общая сумма'
+    )
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name='Дата создания'
     )
     updated_at = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
+        verbose_name='Дата обновления'
     )
+
+    def __str__(self):
+        return f'ID_{self.id} - {self.user}, {self.status}'
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
 
 
 class ProductCollection(models.Model):
-    name = models.CharField(max_length=64)
-    text = models.TextField(blank=True, default='')
-    products = models.ManyToManyField(Product)
+    """Модель Подборки товаров"""
+    name = models.CharField(
+        max_length=64,
+        verbose_name='Название'
+    )
+    text = models.TextField(
+        blank=True,
+        default='',
+        verbose_name='Текст'
+    )
+    products = models.ManyToManyField(
+        Product,
+        verbose_name='Продукты'
+    )
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name='Дата создания'
     )
     updated_at = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
+        verbose_name='Дата обновления'
     )
+
+    def __str__(self):
+        return f'ID_{self.id} - {self.name}'
+
+    class Meta:
+        verbose_name = 'Подборка'
+        verbose_name_plural = 'Подборки'
